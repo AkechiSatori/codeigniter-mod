8a35315fa084a23bafde91a0b2d0139463f79c07
Akechi Satori <i@menhera.io>
2019-01-27 22:55:28 +0800

    remove strict type

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,14 +1,12 @@
 <?php
-class test extends MY_Controller
-{
-    public function w(int $id, string $str = 'meow', bool $type = true, array $arr = [])
-    {
-        return [
-            'code' => 0,
-            'id' => $id,
-            'str' => $str,
-            'type' => $type,
-            'array' => $arr,
-        ];
-    }
+class test extends CI_Controller {
+	public function w($id = 1, $str = 'wwww', $type = false, $arr = []) {
+		return [
+			'code' => 0,
+			'id' => $id,
+			'str' => $str,
+			'type' => $type,
+			'array' => $arr,
+		];
+	}
 }
